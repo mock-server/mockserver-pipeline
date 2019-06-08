fde77b258d8bec5f7ab11211a2d6c0e9596a3264
James Bloom <733179+jamesdbloom@users.noreply.github.com>
2019-06-08 20:42:41 +0100

    script to run kubectl with AWS authenticator

---
+++ terraform/kubectl.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+
+# parse AWS credentials
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+credentials_file="${DIR}/terraform-credentials.dec"
+if [[ -f "$credentials_file" ]]
+then
+  echo "$credentials_file found."
+
+  while IFS='=' read -r key value
+  do
+    key=$(echo $key | tr '.' '_')
+    eval "${key}=\$value"
+  done < "$credentials_file"
+
+else
+  echo "$credentials_file not found."
+fi
+
+eval "AWS_ACCESS_KEY_ID=${awsKeyId} AWS_SECRET_ACCESS_KEY=${awsKeySecret} kubectl --context mockserver-eks ${1}"
